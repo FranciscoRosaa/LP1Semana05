56d3dbb2b80a8e995fb27780fca3ae2f7a3c03d1
Francisco <franciscorosa.work@gmail.com>
2026-03-19 16:26:57 +0000

    Add Damage and CriticalHit methods

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -8,5 +8,44 @@ namespace DungeonStats
         {
             Console.WriteLine("Hello LP!");
         }
+
+        private static int Damage(int attack, int defense)
+        {
+            int stat = attack - defense;
+
+            if (stat > 0)
+            {
+                return stat;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private static int Damage(int attack)
+        {
+            if (attack > 0)
+            {
+                return attack;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private static int CriticalHit(int damage)
+        {
+            if (damage <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return damage + CriticalHit(damage - 1);
+            }
+
+        }
     }
 }
